9ab790b19b8815b6c0e32f10d5251c5e38a66299
Emmett Butler <emmett.butler321@gmail.com>
2013-10-17 18:01:07 -0400

    style

--- merchant_scroll.py
+++ merchant_scroll.py
@@ -46,8 +46,7 @@ def calculate_price(fname, pricemap, price_bracket, include_lands=False):
             print "%s(%s/u): %s" % (line,
                 locale.currency(pricemap[cardname], grouping=True),
                 locale.currency(to_add, grouping=True))
-        print "Total: %s (at %s price bracket)" % (locale.currency(total,
-        grouping=True), price_bracket)
+        print "Total: %s (at %s price bracket)" % (locale.currency(total, grouping=True), price_bracket)
 
 
 if __name__ == "__main__":
